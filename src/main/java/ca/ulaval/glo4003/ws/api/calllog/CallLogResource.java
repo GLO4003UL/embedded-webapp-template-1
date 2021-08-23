c8db4a36774dc388eac61905a63bc458fcd33272
Bruno Gagnon-Adam <bgagnonadam@gmail.com>
2021-08-23 19:02:38 -0400

    Update to Java 13 and dependency updates

--- src/main/java/ca/ulaval/glo4003/ws/api/calllog/CallLogResource.java
+++ src/main/java/ca/ulaval/glo4003/ws/api/calllog/CallLogResource.java
@@ -2,23 +2,19 @@ package ca.ulaval.glo4003.ws.api.calllog;
 
 
 import ca.ulaval.glo4003.ws.api.calllog.dto.CallLogDto;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import java.util.List;
 
-@Path("/telephony/calllogs")
+@Path("/api/telephony/calllogs")
 public interface CallLogResource {
 
-  @GET
-  @Produces(MediaType.APPLICATION_JSON)
-  List<CallLogDto> getCallLogs();
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    List<CallLogDto> getCallLogs();
 
-  @DELETE
-  @Path("{id}")
-  void deleteCallLog(@PathParam("id") String id);
+    @DELETE
+    @Path("{id}")
+    void deleteCallLog(@PathParam("id") String id);
 }
--- src/main/java/ca/ulaval/glo4003/ws/infrastructure/contact/ContactRepositoryInMemory.java
+++ src/main/java/ca/ulaval/glo4003/ws/infrastructure/contact/ContactRepositoryInMemory.java
@@ -3,7 +3,7 @@ package ca.ulaval.glo4003.ws.infrastructure.contact;
 import ca.ulaval.glo4003.ws.domain.contact.Contact;
 import ca.ulaval.glo4003.ws.domain.contact.ContactNotFoundException;
 import ca.ulaval.glo4003.ws.domain.contact.ContactRepository;
-import jersey.repackaged.com.google.common.collect.Lists;
+import com.google.common.collect.Lists;
 
 import java.util.HashMap;
 import java.util.List;
@@ -11,36 +11,36 @@ import java.util.Map;
 
 public class ContactRepositoryInMemory implements ContactRepository {
 
-  private Map<String, Contact> contacts = new HashMap<>();
+    private Map<String, Contact> contacts = new HashMap<>();
 
-  @Override
-  public List<Contact> findAll() {
-      return Lists.newArrayList(contacts.values());
-  }
-
-  @Override
-  public Contact findById(String id) {
-    return contacts.get(id);
-  }
-
-  @Override
-  public void update(Contact contact)
-          throws ContactNotFoundException {
-    Contact foundContact = contacts.get(contact.getId());
-    if (foundContact != null) {
-      contacts.put(contact.getId(), contact);
-    } else {
-      throw new ContactNotFoundException("Contact not found, cannot be updated");
+    @Override
+    public List<Contact> findAll() {
+        return Lists.newArrayList(contacts.values());
     }
-  }
 
-  @Override
-  public void save(Contact contact) {
-    contacts.put(contact.getId(), contact);
-  }
+    @Override
+    public Contact findById(String id) {
+        return contacts.get(id);
+    }
 
-  @Override
-  public void remove(String id) {
-    contacts.remove(id);
-  }
+    @Override
+    public void update(Contact contact)
+            throws ContactNotFoundException {
+        Contact foundContact = contacts.get(contact.getId());
+        if (foundContact != null) {
+            contacts.put(contact.getId(), contact);
+        } else {
+            throw new ContactNotFoundException("Contact not found, cannot be updated");
+        }
+    }
+
+    @Override
+    public void save(Contact contact) {
+        contacts.put(contact.getId(), contact);
+    }
+
+    @Override
+    public void remove(String id) {
+        contacts.remove(id);
+    }
 }
